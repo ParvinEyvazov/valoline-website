fb8c38425d39fe0614b8328be9300d24976a0a22
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2021-08-21 15:28:14 +0300

    landing page updated

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  image_name: string | undefined;
+  number_of_images = 29;
 
   ngOnInit(): void {
+    this.setBackgroundImage();
   }
 
+  setBackgroundImage() {
+    let current_date = Date.now();
+
+    let mod = current_date % this.number_of_images;
+    mod++;
+
+    this.image_name = `background_${mod}.jpg`;
+  }
 }
